4acad2b98e4cf469d4ec88ea89dabcbaccb27a82
Steven Vondruska <vondruska@users.noreply.github.com>
2023-03-04 03:24:27 +0000

    more logging

--- src/PaperlessClient.cs
+++ src/PaperlessClient.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Headers;
 using System.Text;
@@ -7,12 +8,14 @@ namespace SmtpToPaperless
     public class PaperlessClient : IPaperlessClient
     {
         private readonly HttpClient _httpClient;
-        public PaperlessClient(IHttpClientFactory httpClientFactory, IOptions<Configuration> options)
+        public PaperlessClient(IHttpClientFactory httpClientFactory, IOptions<Configuration> options, ILogger<PaperlessClient> logger)
         {
             _httpClient = httpClientFactory.CreateClient();
             _httpClient.BaseAddress = options.Value.PaperlessBaseUrl;
             _httpClient.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(options.Value.PaperlessUsername + ":" + options.Value.PaperlessPassword)));
+
+            logger.LogInformation("Using {0} with username {1}", options.Value.PaperlessBaseUrl, options.Value.PaperlessUsername);
         }
 
         public async Task UploadFileAsync(string fileName, Stream fileToUpload, CancellationToken cancellationToken)
